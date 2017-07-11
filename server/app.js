0af5d25565f3759fabd853038640c0a36c4ac339
Matthew Victor <vicmcnabby@gmail.com>
2017-07-11 11:58:18 -0600

    added get all books route

--- server/app.js
+++ server/app.js
@@ -1,11 +1,13 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-var app = express();
+const app = express();
+
+const books = require('./api/books')
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -13,6 +15,8 @@ app.use(bodyParser.urlencoded({
   extended: false
 }));
 app.use(cookieParser());
+
+app.use("/books", books);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
